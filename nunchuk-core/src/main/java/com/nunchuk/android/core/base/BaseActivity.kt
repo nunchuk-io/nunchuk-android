27117e82ee76511ef400c16e55ea0691b65a148b
Hung Tran <hung.tran@tamara.co>
2021-07-14 08:59:33 +0700

    [AND][Tech] - Disable profiler.

--- nunchuk-core/src/main/java/com/nunchuk/android/core/base/BaseActivity.kt
+++ nunchuk-core/src/main/java/com/nunchuk/android/core/base/BaseActivity.kt
@@ -2,10 +2,8 @@ package com.nunchuk.android.core.base
 
 import android.app.Dialog
 import android.os.Bundle
-import android.os.StrictMode
 import androidx.appcompat.app.AppCompatActivity
 import com.nunchuk.android.arch.R
-import com.nunchuk.android.core.BuildConfig
 import com.nunchuk.android.core.manager.ActivityManager
 import com.nunchuk.android.nav.NunchukNavigator
 import com.nunchuk.android.utils.DisposableManager
@@ -41,7 +39,6 @@ abstract class BaseActivity : AppCompatActivity(), HasAndroidInjector {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
-        setupStrictMode()
         super.onCreate(savedInstanceState)
         overridePendingTransition(R.anim.enter, R.anim.exit)
         ActivityManager.instance.add(this)
@@ -53,27 +50,6 @@ abstract class BaseActivity : AppCompatActivity(), HasAndroidInjector {
         DisposableManager.instance.dispose()
         ActivityManager.instance.remove(this)
         dialog?.cancel()
-    }
-
-    private fun setupStrictMode() {
-        if (BuildConfig.DEBUG) {
-            StrictMode.setThreadPolicy(
-                StrictMode.ThreadPolicy.Builder()
-                    .detectCustomSlowCalls()
-                    .penaltyLog()
-                    .build()
-            )
-            StrictMode.setVmPolicy(
-                StrictMode.VmPolicy.Builder()
-                    .detectLeakedSqlLiteObjects()
-                    .detectLeakedClosableObjects()
-                    .detectActivityLeaks()
-                    .detectLeakedRegistrationObjects()
-                    .detectFileUriExposure()
-                    .penaltyLog()
-                    .build()
-            )
-        }
     }
 
 }
