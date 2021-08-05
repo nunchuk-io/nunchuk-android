1c9e0a3e5e3c5cf63140303d2e721c652b7c1289
Hung Tran <hung.tran@nunchuk.io>
2021-08-05 16:30:30 +0000

    [NUN-157]

--- nunchuk-domain/src/main/java/com/nunchuk/android/usecase/CheckMnemonicUseCase.kt
+++ nunchuk-domain/src/main/java/com/nunchuk/android/usecase/CheckMnemonicUseCase.kt
@@ -1,17 +1,18 @@
 package com.nunchuk.android.usecase
 
-import com.nunchuk.android.model.Result
 import com.nunchuk.android.nativelib.NunchukNativeSdk
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 interface CheckMnemonicUseCase {
-    suspend fun execute(mnemonic: String): Result<Boolean>
+    fun execute(mnemonic: String): Flow<Boolean>
 }
 
 internal class CheckMnemonicUseCaseImpl @Inject constructor(
     private val nativeSdk: NunchukNativeSdk
-) : BaseUseCase(), CheckMnemonicUseCase {
+) : CheckMnemonicUseCase {
 
-    override suspend fun execute(mnemonic: String) = exe { nativeSdk.checkMnemonic(mnemonic) }
+    override fun execute(mnemonic: String) = flow { emit(nativeSdk.checkMnemonic(mnemonic)) }
 
 }
