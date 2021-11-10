b5a3553f38c3b280e6d519b5c7dcde5031036a27
Hung Tran <hung.tran@tamara.co>
2021-11-10 19:51:06 +0700

    - Update main/testnet blockchain url.

--- nunchuk-core/src/main/java/com/nunchuk/android/core/domain/GetBlockchainExplorerUrlUseCase.kt
+++ nunchuk-core/src/main/java/com/nunchuk/android/core/domain/GetBlockchainExplorerUrlUseCase.kt
@@ -1,12 +1,13 @@
 package com.nunchuk.android.core.domain
 
-import com.nunchuk.android.model.AppSettings
+import com.nunchuk.android.core.util.isMainNet
 import com.nunchuk.android.type.Chain
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
-const val URL_TEMPLATE = "https://blockstream.info%1s/tx/%2s"
+const val TESTNET_URL_TEMPLATE = "https://blockstream.info/testnet/tx/"
+const val MAINNET_URL_TEMPLATE = "https://blockstream.info/tx/"
 
 interface GetBlockchainExplorerUrlUseCase {
     fun execute(txId: String): Flow<String>
@@ -16,12 +17,15 @@ internal class GetBlockchainExplorerUrlUseCaseImpl @Inject constructor(
     private val appSettingsUseCase: GetAppSettingUseCase
 ) : GetBlockchainExplorerUrlUseCase {
 
-    override fun execute(txId: String) = appSettingsUseCase.execute().map { formatUrl(it, txId) }
+    override fun execute(txId: String) = appSettingsUseCase.execute().map { formatUrl(it.chain, txId) }
 
-    private fun formatUrl(
-        settings: AppSettings,
-        txId: String
-    ) = String.format(URL_TEMPLATE, (if (settings.chain == Chain.TESTNET) "/testnet" else ""), txId)
+    private fun formatUrl(chain: Chain, txId: String) = getTemplate(chain) + txId
+
+    private fun getTemplate(chain: Chain) = if (chain.isMainNet()) {
+        MAINNET_URL_TEMPLATE
+    } else {
+        (TESTNET_URL_TEMPLATE)
+    }
 
 }
 
