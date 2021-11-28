1763e41427d3814881d926293d45450e93f5ca37
Hung Tran <hung.tran@nunchuk.io>
2021-11-28 10:25:36 +0000

    [NUN-731] Truncated transaction address.

--- nunchuk-core/src/main/java/com/nunchuk/android/core/util/TransactionExtensions.kt
+++ nunchuk-core/src/main/java/com/nunchuk/android/core/util/TransactionExtensions.kt
@@ -10,4 +10,27 @@ fun TransactionStatus.hadBroadcast() = this == CONFIRMED || this == NETWORK_REJE
 
 fun TransactionStatus.isPending() = this == PENDING_SIGNATURES || this == READY_TO_BROADCAST
 
-fun Transaction.getPendingSignatures() = 0.coerceAtLeast(m - signers.count { it.value })
+fun Transaction.getPendingSignatures() = 0.coerceAtLeast(m - signers.count(Map.Entry<String, Boolean>::value))
+
+fun List<Transaction>.sorted(): List<Transaction> {
+    return sortedWith(comparatorTransaction)
+}
+
+private val comparatorTransaction = Comparator<Transaction> { t1, t2 ->
+    when {
+        t1.txId == t2.replacedByTxid && t1.status == CONFIRMED && t2.status == REPLACED -> -1
+        t1.txId != t2.replacedByTxid && t1.status == CONFIRMED && t2.status == REPLACED -> 1
+        t1.status == CONFIRMED && t2.status == CONFIRMED -> t2.blockTime.compareTo(t1.blockTime)
+        t1.status.compareTo(t2.status) == 0 -> t2.blockTime.compareTo(t1.blockTime)
+        else -> t1.status.compareTo(t2.status)
+    }
+}
+
+fun String.truncatedAddress(): String = if (length < PREFIX_LENGTH + SUFFIX_LENGTH) {
+    this
+} else {
+    "${take(PREFIX_LENGTH)}...${takeLast(SUFFIX_LENGTH)}"
+}
+
+internal const val PREFIX_LENGTH = 5
+internal const val SUFFIX_LENGTH = 4
--- nunchuk-domain/src/main/java/com/nunchuk/android/model/TransactionExt.kt
+++ nunchuk-domain/src/main/java/com/nunchuk/android/model/TransactionExt.kt
@@ -1,19 +1,7 @@
 package com.nunchuk.android.model
 
-import com.nunchuk.android.type.TransactionStatus
-
-data class TransactionExt(val walletId: String, val initEventId: String, val transaction: Transaction)
-
-fun List<Transaction>.sorted(): List<Transaction> {
-    return sortedWith(comparatorTransaction)
-}
-
-private val comparatorTransaction = Comparator<Transaction> { t1, t2 ->
-    when {
-        t1.txId == t2.replacedByTxid && t1.status == TransactionStatus.CONFIRMED && t2.status == TransactionStatus.REPLACED -> -1
-        t1.txId != t2.replacedByTxid && t1.status == TransactionStatus.CONFIRMED && t2.status == TransactionStatus.REPLACED -> 1
-        t1.status == TransactionStatus.CONFIRMED && t2.status == TransactionStatus.CONFIRMED -> t2.blockTime.compareTo(t1.blockTime)
-        t1.status.compareTo(t2.status) == 0 -> t2.blockTime.compareTo(t1.blockTime)
-        else -> t1.status.compareTo(t2.status)
-    }
-}
+data class TransactionExt(
+    val walletId: String,
+    val initEventId: String,
+    val transaction: Transaction
+)
