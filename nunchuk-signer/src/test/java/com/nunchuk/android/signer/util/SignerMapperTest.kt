8d75df3ce8ef6d309a8a81459c54291d1c077c48
Hung Tran <hung.tran@nunchuk.io>
2021-11-28 11:27:25 +0000

    [NUN-737] Fix font weight for small badges.

--- nunchuk-signer/src/test/java/com/nunchuk/android/signer/util/SignerMapperTest.kt
+++ nunchuk-signer/src/test/java/com/nunchuk/android/signer/util/SignerMapperTest.kt
@@ -1,15 +1,15 @@
 package com.nunchuk.android.signer.util
 
-import org.junit.Assert.*
+import com.nunchuk.android.core.signer.toSigner
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
 internal class SignerMapperTest {
 
-    @org.junit.Test
+    @Test
     fun parse() {
-        val signerSpec =
-            "[ABCD1234/48h/0h/2h]pub661MyMwAqRbcG8Zah6TcX3QpP5yJApaXcyLK8CJcZkuYjczivsHxVL5qm9cw8BYLYehgFeddK5WrxhntpcvqJKTVg96dUVL9P7hZ7Kcvqvd"
-        val signerInput = signerSpec.toSigner()
-        assertEquals("ABCD1234", signerInput.fingerPrint)
+        val signerInput = "[abcd1234/48h/0h/2h]pub661MyMwAqRbcG8Zah6TcX3QpP5yJApaXcyLK8CJcZkuYjczivsHxVL5qm9cw8BYLYehgFeddK5WrxhntpcvqJKTVg96dUVL9P7hZ7Kcvqvd".toSigner()
+        assertEquals("abcd1234", signerInput.fingerPrint)
         assertEquals("m/48h/0h/2h", signerInput.derivationPath)
         assertEquals(
             "pub661MyMwAqRbcG8Zah6TcX3QpP5yJApaXcyLK8CJcZkuYjczivsHxVL5qm9cw8BYLYehgFeddK5WrxhntpcvqJKTVg96dUVL9P7hZ7Kcvqvd",
@@ -17,12 +17,10 @@ internal class SignerMapperTest {
         )
     }
 
-    @org.junit.Test
+    @Test
     fun parse1() {
-        val signerSpec =
-            "[ABCD1234/48h/0h/2h]pub661MyMwAqRbcG8Zah6TcX3QpP5yJApaXcyLK8CJcZkuYjczivsHxVL5qm9cw8BYLYehgFeddK5WrxhntpcvqJKTVg96dUVL9P7hZ7Kcvqvd/1*/0*"
-        val signerInput = signerSpec.toSigner()
-        assertEquals("ABCD1234", signerInput.fingerPrint)
+        val signerInput = "[abcd1234/48h/0h/2h]pub661MyMwAqRbcG8Zah6TcX3QpP5yJApaXcyLK8CJcZkuYjczivsHxVL5qm9cw8BYLYehgFeddK5WrxhntpcvqJKTVg96dUVL9P7hZ7Kcvqvd/1*/0*".toSigner()
+        assertEquals("abcd1234", signerInput.fingerPrint)
         assertEquals("m/48h/0h/2h", signerInput.derivationPath)
         assertEquals(
             "pub661MyMwAqRbcG8Zah6TcX3QpP5yJApaXcyLK8CJcZkuYjczivsHxVL5qm9cw8BYLYehgFeddK5WrxhntpcvqJKTVg96dUVL9P7hZ7Kcvqvd",
@@ -30,12 +28,10 @@ internal class SignerMapperTest {
         )
     }
 
-    @org.junit.Test
+    @Test
     fun parse2() {
-        val signerSpec =
-            "[ABCD1234/44'/1'/0'/0]xpub6EVnUYrCsgejjYzXeMcGV3sx6z3jAMKLsMqBzQ9MugqKM4jsEzVkSiRc5BrpSQ3JFzV2vYSqHs24itD1JvN3vEqWYTLi3QXkEyMGNd4eTSR"
-        val signerInput = signerSpec.toSigner()
-        assertEquals("ABCD1234", signerInput.fingerPrint)
+        val signerInput = "[abcd1234/44'/1'/0'/0]xpub6EVnUYrCsgejjYzXeMcGV3sx6z3jAMKLsMqBzQ9MugqKM4jsEzVkSiRc5BrpSQ3JFzV2vYSqHs24itD1JvN3vEqWYTLi3QXkEyMGNd4eTSR".toSigner()
+        assertEquals("abcd1234", signerInput.fingerPrint)
         assertEquals("m/44'/1'/0'/0", signerInput.derivationPath)
         assertEquals(
             "xpub6EVnUYrCsgejjYzXeMcGV3sx6z3jAMKLsMqBzQ9MugqKM4jsEzVkSiRc5BrpSQ3JFzV2vYSqHs24itD1JvN3vEqWYTLi3QXkEyMGNd4eTSR",
