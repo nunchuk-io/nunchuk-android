c51eb27f718da360e1ee5f63f08e99cc6b15699d
Hung Tran <hung.tran@nunchuk.io>
2021-12-08 00:53:30 +0000

    [NUN-797] [Transaction] Incorrect date format

--- nunchuk-core/src/main/java/com/nunchuk/android/core/util/Util.kt
+++ nunchuk-core/src/main/java/com/nunchuk/android/core/util/Util.kt
@@ -25,14 +25,12 @@ var BLOCKCHAIN_STATUS: ConnectionStatus? = null
 const val SATOSHI_BTC_EXCHANGE_RATE = 0.00000001
 const val BTC_SATOSHI_EXCHANGE_RATE = 100000000
 
-fun Long.formatDate(): String = SimpleDateFormat("dd/MM/yyyy 'at' HH:mm aaa", Locale.US).format(Date(this * 1000))
+fun Long.formatDate(): String = SimpleDateFormat("MM/dd/yyyy 'at' HH:mm aaa", Locale.US).format(Date(this * 1000))
 
 fun Transaction.getFormatDate(): String = if (blockTime <= 0) "--/--/--" else (blockTime).formatDate()
 
 fun String.fromMxcUriToMatrixDownloadUrl(): String {
-    if (this.isEmpty()) {
-        return ""
-    }
+    if (this.isEmpty()) return ""
 
     // Sample: https://matrix.nunchuk.io/_matrix/media/r0/download/nunchuk.io/occyhYuhbbpkHNbJLZwwdtuf
     val contentUriInfo = this.removePrefix("mxc://").split("/")
