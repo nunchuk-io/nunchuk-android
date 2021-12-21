fd45cef9cd8b79d4c73325b475c6fd5ea2841a2b
Hung Tran <hung.tran@nunchuk.io>
2021-12-21 02:32:44 +0000

    [NUN-927] Trim signer spec.

--- nunchuk-signer/src/test/java/com/nunchuk/android/signer/util/SignerMapperTest.kt
+++ nunchuk-signer/src/test/java/com/nunchuk/android/signer/util/SignerMapperTest.kt
@@ -18,8 +18,8 @@ internal class SignerMapperTest {
     }
 
     @Test
-    fun parse1() {
-        val signerInput = "[abcd1234/48h/0h/2h]pub661MyMwAqRbcG8Zah6TcX3QpP5yJApaXcyLK8CJcZkuYjczivsHxVL5qm9cw8BYLYehgFeddK5WrxhntpcvqJKTVg96dUVL9P7hZ7Kcvqvd/1*/0*".toSigner()
+    fun toSignerWithSpace() {
+        val signerInput = "[abcd1234/48h/0h/2h]pub661MyMwAqRbcG8Zah6TcX3QpP5yJApaXcyLK8CJcZkuYjczivsHxVL5qm9cw8BYLYehgFeddK5WrxhntpcvqJKTVg96dUVL9P7hZ7Kcvqvd ".toSigner()
         assertEquals("abcd1234", signerInput.fingerPrint)
         assertEquals("m/48h/0h/2h", signerInput.derivationPath)
         assertEquals(
@@ -29,12 +29,23 @@ internal class SignerMapperTest {
     }
 
     @Test
-    fun parse2() {
-        val signerInput = "[abcd1234/44'/1'/0'/0]xpub6EVnUYrCsgejjYzXeMcGV3sx6z3jAMKLsMqBzQ9MugqKM4jsEzVkSiRc5BrpSQ3JFzV2vYSqHs24itD1JvN3vEqWYTLi3QXkEyMGNd4eTSR".toSigner()
+    fun toSignerWithLineBreak() {
+        val signerInput = "[abcd1234/48h/0h/2h]pub661MyMwAqRbcG8Zah6TcX3QpP5yJApaXcyLK8CJcZkuYjczivsHxVL5qm9cw8BYLYehgFeddK5WrxhntpcvqJKTVg96dUVL9P7hZ7Kcvqvd\n".toSigner()
         assertEquals("abcd1234", signerInput.fingerPrint)
-        assertEquals("m/44'/1'/0'/0", signerInput.derivationPath)
+        assertEquals("m/48h/0h/2h", signerInput.derivationPath)
         assertEquals(
-            "xpub6EVnUYrCsgejjYzXeMcGV3sx6z3jAMKLsMqBzQ9MugqKM4jsEzVkSiRc5BrpSQ3JFzV2vYSqHs24itD1JvN3vEqWYTLi3QXkEyMGNd4eTSR",
+            "pub661MyMwAqRbcG8Zah6TcX3QpP5yJApaXcyLK8CJcZkuYjczivsHxVL5qm9cw8BYLYehgFeddK5WrxhntpcvqJKTVg96dUVL9P7hZ7Kcvqvd",
+            signerInput.xpub
+        )
+    }
+
+    @Test
+    fun toSignerExtra() {
+        val signerInput = "[abcd1234/48h/0h/2h]pub661MyMwAqRbcG8Zah6TcX3QpP5yJApaXcyLK8CJcZkuYjczivsHxVL5qm9cw8BYLYehgFeddK5WrxhntpcvqJKTVg96dUVL9P7hZ7Kcvqvd/1*/0*".toSigner()
+        assertEquals("abcd1234", signerInput.fingerPrint)
+        assertEquals("m/48h/0h/2h", signerInput.derivationPath)
+        assertEquals(
+            "pub661MyMwAqRbcG8Zah6TcX3QpP5yJApaXcyLK8CJcZkuYjczivsHxVL5qm9cw8BYLYehgFeddK5WrxhntpcvqJKTVg96dUVL9P7hZ7Kcvqvd",
             signerInput.xpub
         )
     }
