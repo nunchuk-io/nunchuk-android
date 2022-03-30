1e24d55bef4540d22e7dbe4b9058b0cc64fed3f4
Kieu Duc <duc.kieu8795@gmail.com>
2022-03-30 18:11:55 +0700

    [NUN-1033]: Display unit copy + design tweak

--- nunchuk-settings/src/main/java/com/nunchuk/android/settings/AccountFragment.kt
+++ nunchuk-settings/src/main/java/com/nunchuk/android/settings/AccountFragment.kt
@@ -19,6 +19,8 @@ import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import com.nunchuk.android.core.base.BaseFragment
+import com.nunchuk.android.core.entities.CURRENT_DISPLAY_UNIT_TYPE
+import com.nunchuk.android.core.entities.SAT
 import com.nunchuk.android.core.guestmode.SignInModeHolder
 import com.nunchuk.android.core.guestmode.isGuestMode
 import com.nunchuk.android.core.util.*
@@ -73,6 +75,11 @@ internal class AccountFragment : BaseFragment<FragmentAccountBinding>() {
         binding.signIn.isVisible = isGuestMode
         binding.signUp.isVisible = isGuestMode
         binding.accountSettings.isVisible = !isGuestMode
+
+        binding.unit.text = when(CURRENT_DISPLAY_UNIT_TYPE) {
+            SAT -> getString(R.string.nc_settings_unit, getString(R.string.nc_currency_sat))
+            else -> getString(R.string.nc_settings_unit, getString(R.string.nc_currency_btc))
+        }
     }
 
     private fun openAboutScreen() {
