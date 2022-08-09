44d6d5d9c7ae50092e472218beb6b5b84bc2bcd3
Jayce Luong <jayce@nunchuk.io>
2022-08-09 19:57:25 +0700

    #Feature [NUN-1741] Update Blockstream to mempool

--- nunchuk-core/src/main/java/com/nunchuk/android/core/constants/Constants.kt
+++ nunchuk-core/src/main/java/com/nunchuk/android/core/constants/Constants.kt
@@ -5,8 +5,8 @@ object Constants {
     const val TEST_NET_HOST = "testnet.nunchuk.io:50001"
     const val SIG_NET_HOST = "signet.nunchuk.io:50002"
 
-    const val TESTNET_URL_TEMPLATE = "https://blockstream.info/testnet/tx/"
-    const val MAINNET_URL_TEMPLATE = "https://blockstream.info/tx/"
+    const val TESTNET_URL_TEMPLATE = "https://mempool.space/testnet/tx/"
+    const val MAINNET_URL_TEMPLATE = "https://mempool.space/tx/"
     const val GLOBAL_SIGNET_EXPLORER = "https://explorer.bc-2.jp"
 
 }
