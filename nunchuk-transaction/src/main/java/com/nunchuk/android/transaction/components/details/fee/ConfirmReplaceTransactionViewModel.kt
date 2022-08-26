e36f38a0ca48faff4a5f75b5765e87d7908b0a3d
Jayce Luong <jayce@nunchuk.io>
2022-08-26 13:34:50 +0000

    #Feature [NUN-1774] Replace by fee new UI

---
+++ nunchuk-transaction/src/main/java/com/nunchuk/android/transaction/components/details/fee/ConfirmReplaceTransactionViewModel.kt
@@ -0,0 +1,60 @@
+package com.nunchuk.android.transaction.components.details.fee
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.nunchuk.android.ReplaceTransactionUseCase
+import com.nunchuk.android.model.Result
+import com.nunchuk.android.model.Transaction
+import com.nunchuk.android.transaction.components.send.confirmation.toManualFeeRate
+import com.nunchuk.android.usecase.DraftTransactionUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ConfirmReplaceTransactionViewModel @Inject constructor(
+    private val replaceTransactionUseCase: ReplaceTransactionUseCase,
+    private val draftTransactionUseCase: DraftTransactionUseCase
+) : ViewModel() {
+    private val _event = MutableSharedFlow<ReplaceFeeEvent>()
+    private val _state = MutableStateFlow<ConfirmReplaceTransactionState?>(null)
+    val state = _state.asStateFlow().filterIsInstance<ConfirmReplaceTransactionState>()
+    val event = _event.asSharedFlow()
+
+    fun draftTransaction(walletId: String, oldTx: Transaction, newFee: Int) {
+        viewModelScope.launch {
+            delay(150) // work around shared flow not show loading
+            _event.emit(ReplaceFeeEvent.Loading(true))
+            when (val result = draftTransactionUseCase.execute(
+                walletId = walletId,
+                inputs = oldTx.inputs,
+                outputs = oldTx.userOutputs.associate { it.first to it.second },
+                subtractFeeFromAmount = oldTx.subtractFeeFromAmount,
+                feeRate = newFee.toManualFeeRate()
+            )) {
+                is Result.Success -> _state.value = ConfirmReplaceTransactionState(result.data)
+                is Result.Error -> {
+                    _event.emit(ReplaceFeeEvent.ShowError(result.exception))
+                }
+            }
+            _event.emit(ReplaceFeeEvent.Loading(false))
+        }
+    }
+
+    fun replaceTransaction(walletId: String, txId: String, newFee: Int) {
+        viewModelScope.launch {
+            _event.emit(ReplaceFeeEvent.Loading(true))
+            val result = replaceTransactionUseCase(ReplaceTransactionUseCase.Data(walletId, txId, newFee))
+            _event.emit(ReplaceFeeEvent.Loading(false))
+            if (result.isSuccess) {
+                _event.emit(ReplaceFeeEvent.ReplaceTransactionSuccess(result.getOrThrow().txId))
+            } else {
+                _event.emit(ReplaceFeeEvent.ShowError(result.exceptionOrNull()))
+            }
+        }
+    }
+}
+
+data class ConfirmReplaceTransactionState(val transaction: Transaction)
--- nunchuk-transaction/src/main/java/com/nunchuk/android/transaction/components/details/fee/ReplaceFeeActivity.kt
+++ nunchuk-transaction/src/main/java/com/nunchuk/android/transaction/components/details/fee/ReplaceFeeActivity.kt
@@ -1,29 +1,17 @@
 package com.nunchuk.android.transaction.components.details.fee
 
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.flowWithLifecycle
-import androidx.lifecycle.lifecycleScope
 import com.nunchuk.android.core.base.BaseActivity
-import com.nunchuk.android.core.util.orUnknownError
-import com.nunchuk.android.model.EstimateFeeRates
-import com.nunchuk.android.transaction.R
-import com.nunchuk.android.transaction.components.send.fee.toFeeRate
+import com.nunchuk.android.model.Transaction
 import com.nunchuk.android.transaction.databinding.ActivityReplaceByFeeBinding
-import com.nunchuk.android.utils.safeManualFee
-import com.nunchuk.android.widget.NCToastMessage
 import com.nunchuk.android.widget.util.setLightStatusBar
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ReplaceFeeActivity : BaseActivity<ActivityReplaceByFeeBinding>() {
-    private val viewModel: ReplaceFeeViewModel by viewModels()
-    private val args: ReplaceFeeArgs by lazy { ReplaceFeeArgs.deserializeFrom(intent) }
 
     override fun initializeBinding(): ActivityReplaceByFeeBinding {
         return ActivityReplaceByFeeBinding.inflate(layoutInflater)
@@ -32,56 +20,11 @@ class ReplaceFeeActivity : BaseActivity<ActivityReplaceByFeeBinding>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setLightStatusBar()
-        observer()
-        initViews()
-    }
-
-    private fun initViews() {
-        binding.toolbar.setNavigationOnClickListener {
-            finish()
-        }
-        binding.btnContinue.setOnClickListener {
-            val newFee = binding.feeRateInput.text.safeManualFee()
-            if (viewModel.validateFeeRate(newFee)) {
-                viewModel.replaceTransaction(args.walletId, args.txId, newFee)
-            } else {
-                NCToastMessage(this).showError(getString(R.string.nc_input_fee_invalid_error))
-            }
-        }
-    }
-
-    private fun observer() {
-        lifecycleScope.launchWhenStarted {
-            viewModel.state.flowWithLifecycle(lifecycle, Lifecycle.State.STARTED)
-                .collect {
-                    bindEstimateFeeRates(it.estimateFeeRates)
-                }
-        }
-
-        lifecycleScope.launchWhenStarted {
-            viewModel.event.flowWithLifecycle(lifecycle, Lifecycle.State.STARTED)
-                .collect {
-                    when (it) {
-                        is ReplaceFeeEvent.Loading -> showOrHideLoading(it.isLoading)
-                        is ReplaceFeeEvent.ReplaceTransactionSuccess -> {
-                            setResult(Activity.RESULT_OK, ReplaceFeeArgs(args.walletId, it.newTxId).buildIntent(this@ReplaceFeeActivity))
-                            finish()
-                        }
-                        is ReplaceFeeEvent.ShowError -> NCToastMessage(this@ReplaceFeeActivity).showError(it.e?.message.orUnknownError())
-                    }
-                }
-        }
-    }
-
-    private fun bindEstimateFeeRates(estimateFeeRates: EstimateFeeRates) {
-        binding.priorityRateValue.text = estimateFeeRates.priorityRate.toFeeRate()
-        binding.standardRateValue.text = estimateFeeRates.standardRate.toFeeRate()
-        binding.economicalRateValue.text = estimateFeeRates.economicRate.toFeeRate()
     }
 
     companion object {
-        fun start(launcher: ActivityResultLauncher<Intent>, context: Context, walletId: String, txId: String) {
-            launcher.launch(ReplaceFeeArgs(walletId, txId).buildIntent(context))
+        fun start(launcher: ActivityResultLauncher<Intent>, context: Context, walletId: String, transaction: Transaction) {
+            launcher.launch(ReplaceFeeArgs(walletId, transaction).buildIntent(context))
         }
     }
 }
