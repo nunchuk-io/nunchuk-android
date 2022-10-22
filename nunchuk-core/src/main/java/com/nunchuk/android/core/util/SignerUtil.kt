4db91743a0ee541e95106b7c0a943a84f9240ac5
Jayce Luong <jayce@nunchuk.io>
2022-10-22 21:40:08 +0700

    #Feature [NUN-2071] Update software signer icon

--- nunchuk-core/src/main/java/com/nunchuk/android/core/util/SignerUtil.kt
+++ nunchuk-core/src/main/java/com/nunchuk/android/core/util/SignerUtil.kt
@@ -27,7 +27,7 @@ fun SignerType.toReadableDrawable(context: Context, isPrimaryKey: Boolean = fals
     if (isPrimaryKey) return ContextCompat.getDrawable(context, R.drawable.ic_signer_type_primary_key_small)
     return when (this) {
         AIRGAP, COLDCARD_NFC -> ContextCompat.getDrawable(context, R.drawable.ic_air_signer_small)
-        SOFTWARE -> ContextCompat.getDrawable(context, R.drawable.ic_software_key)
+        SOFTWARE -> ContextCompat.getDrawable(context, R.drawable.ic_logo_dark_small)
         HARDWARE -> ContextCompat.getDrawable(context, R.drawable.ic_signer_type_wired)
         FOREIGN_SOFTWARE -> ContextCompat.getDrawable(context, R.drawable.ic_logo_dark_small)
         NFC -> ContextCompat.getDrawable(context, R.drawable.ic_nfc_card)
