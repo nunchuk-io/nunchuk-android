237b24396ec6e42ed428e6d82fe67cd90bfd6b23
Jayce Luong <jayce@nunchuk.io>
2023-06-22 14:15:51 +0700

    Add Nunchuk 2023 license for all classes

--- nunchuk-signer/src/main/java/com/nunchuk/android/signer/components/details/SignerInfoFragment.kt
+++ nunchuk-signer/src/main/java/com/nunchuk/android/signer/components/details/SignerInfoFragment.kt
@@ -1,19 +1,19 @@
 /**************************************************************************
- * requireActivity() file is part of the Nunchuk software (https://nunchuk.io/)        *							          *
- * Copyright (C) 2022 Nunchuk								              *
+ * This file is part of the Nunchuk software (https://nunchuk.io/)        *
+ * Copyright (C) 2022, 2023 Nunchuk                                       *
  *                                                                        *
- * requireActivity() program is free software; you can redistribute it and/or          *
+ * This program is free software; you can redistribute it and/or          *
  * modify it under the terms of the GNU General Public License            *
  * as published by the Free Software Foundation; either version 3         *
  * of the License, or (at your option) any later version.                 *
  *                                                                        *
- * requireActivity() program is distributed in the hope that it will be useful,        *
+ * This program is distributed in the hope that it will be useful,        *
  * but WITHOUT ANY WARRANTY; without even the implied warranty of         *
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
  * GNU General Public License for more details.                           *
  *                                                                        *
  * You should have received a copy of the GNU General Public License      *
- * along with requireActivity() program.  If not, see <http://www.gnu.org/licenses/>.  *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
  *                                                                        *
  **************************************************************************/
 
