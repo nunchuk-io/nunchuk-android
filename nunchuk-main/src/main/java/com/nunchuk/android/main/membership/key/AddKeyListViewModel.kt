f37df8df2ae998b3356d0836f47abe66f9b989f3
Jayce Luong <jayce@nunchuk.io>
2023-06-29 09:57:02 +0700

    [Coldcard] Select existing coldcard when creating assisted wallet

--- nunchuk-main/src/main/java/com/nunchuk/android/main/membership/key/AddKeyListViewModel.kt
+++ nunchuk-main/src/main/java/com/nunchuk/android/main/membership/key/AddKeyListViewModel.kt
@@ -164,7 +164,7 @@ class AddKeyListViewModel @Inject constructor(
         viewModelScope.launch {
             val hasTag =
                 signer.tags.any { it == SignerTag.SEEDSIGNER || it == SignerTag.KEYSTONE || it == SignerTag.PASSPORT || it == SignerTag.JADE }
-            if (hasTag) {
+            if (signer.type == SignerType.COLDCARD_NFC || hasTag) {
                 saveMembershipStepUseCase(
                     MembershipStepInfo(
                         step = membershipStepManager.currentStep
