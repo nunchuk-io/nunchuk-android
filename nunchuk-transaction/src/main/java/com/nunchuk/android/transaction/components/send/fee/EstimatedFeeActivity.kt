4fbf9d0a788847ce11aa26c0cdf5784e18dbb953
Jayce Luong <jayce@nunchuk.io>
2023-07-29 10:44:36 +0700

    [NUN-3540] Effective fee rate show 2 digits after dot

--- nunchuk-transaction/src/main/java/com/nunchuk/android/transaction/components/send/fee/EstimatedFeeActivity.kt
+++ nunchuk-transaction/src/main/java/com/nunchuk/android/transaction/components/send/fee/EstimatedFeeActivity.kt
@@ -161,7 +161,7 @@ class EstimatedFeeActivity : BaseActivity<ActivityTransactionEstimateFeeBinding>
 
         binding.tvEffectiveFee.isVisible = state.cpfpFee.value > 0
         binding.manualFeeDesc.isVisible = binding.tvEffectiveFee.isVisible
-        binding.tvEffectiveFee.text = getString(R.string.nc_transaction_effective_fee_rate, (state.cpfpFee.value.toDouble() / 1000.0).formatDecimal())
+        binding.tvEffectiveFee.text = getString(R.string.nc_transaction_effective_fee_rate, (state.cpfpFee.value.toDouble() / 1000.0).formatDecimal(maxFractionDigits = USD_FRACTION_DIGITS))
 
         if (state.subtractFeeFromAmount) {
             bindSubtotal(viewModel.getOutputAmount())
