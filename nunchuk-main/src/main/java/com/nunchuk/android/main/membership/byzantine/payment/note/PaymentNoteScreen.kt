bfb6b3c1a7733ed7024e7097f847250be12a96d0
Jayce Luong <jayce@nunchuk.io>
2023-12-06 14:14:16 +0700

    [NUN-4621] Missing Skip button on Add transaction note screen

--- nunchuk-main/src/main/java/com/nunchuk/android/main/membership/byzantine/payment/note/PaymentNoteScreen.kt
+++ nunchuk-main/src/main/java/com/nunchuk/android/main/membership/byzantine/payment/note/PaymentNoteScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -54,13 +55,25 @@ fun PaymentNoteScreen(
                 )
             },
             bottomBar = {
-                NcPrimaryDarkButton(
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .fillMaxWidth(),
-                    onClick = openSummaryScreen,
-                ) {
-                    Text(text = stringResource(R.string.nc_text_continue))
+                Column {
+                    NcPrimaryDarkButton(
+                        modifier = Modifier
+                            .padding(horizontal = 16.dp)
+                            .fillMaxWidth(),
+                        onClick = openSummaryScreen,
+                        enabled = note.isNotEmpty(),
+                    ) {
+                        Text(text = stringResource(R.string.nc_text_continue))
+                    }
+
+                    TextButton(
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .fillMaxWidth(),
+                        onClick = openSummaryScreen,
+                    ) {
+                        Text(text = stringResource(R.string.nc_text_skip))
+                    }
                 }
             },
         ) { innerPadding ->
