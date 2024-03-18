87bfb5d3cfe7de7e5aaec8395f12ea65484ac7dc
Jayce Luong <jayce@nunchuk.io>
2024-03-18 14:30:48 +0700

    NUN-5140: Onboard UI issues

--- nunchuk-app/src/main/java/com/nunchuk/android/app/onboard/hotwallet/HotWalletIntroScreen.kt
+++ nunchuk-app/src/main/java/com/nunchuk/android/app/onboard/hotwallet/HotWalletIntroScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
@@ -60,7 +61,7 @@ private fun HotWalletIntroContent(
     openServiceTab: () -> Unit = {},
 ) {
     Scaffold(
-        modifier = modifier,
+        modifier = modifier.systemBarsPadding(),
         topBar = {
             NcImageAppBar(
                 backgroundRes = R.drawable.bg_hot_wallet
