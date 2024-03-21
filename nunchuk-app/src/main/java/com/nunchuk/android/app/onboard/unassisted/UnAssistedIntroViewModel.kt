b77c233cd5a42e0fa7d2a16e896bf75a55c05f10
Jayce Luong <jayce@nunchuk.io>
2024-03-21 15:31:31 +0700

    Onboard: Fix UI issues
    (cherry picked from commit c32cd717c78194d1ff90cf576c03991061132131)

--- nunchuk-app/src/main/java/com/nunchuk/android/app/onboard/unassisted/UnAssistedIntroViewModel.kt
+++ nunchuk-app/src/main/java/com/nunchuk/android/app/onboard/unassisted/UnAssistedIntroViewModel.kt
@@ -2,11 +2,14 @@ package com.nunchuk.android.app.onboard.unassisted
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.nunchuk.android.core.domain.membership.GetLocalMembershipPlanFlowUseCase
 import com.nunchuk.android.core.profile.SetOnBoardUseCase
+import com.nunchuk.android.model.MembershipPlan
 import com.nunchuk.android.usecase.wallet.CreateHotWalletUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import timber.log.Timber
@@ -16,9 +19,20 @@ import javax.inject.Inject
 class UnAssistedIntroViewModel @Inject constructor(
     private val createHotWalletUseCase: CreateHotWalletUseCase,
     private val setOnBoardUseCase: SetOnBoardUseCase,
+    private val getLocalMembershipPlanFlowUseCase: GetLocalMembershipPlanFlowUseCase,
 ) : ViewModel() {
     private val _state = MutableStateFlow(UnAssistedIntroState())
     val state = _state.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            getLocalMembershipPlanFlowUseCase(Unit)
+                .map { it.getOrElse { MembershipPlan.NONE } }
+                .collect {
+                    _state.update { state -> state.copy(plan = it) }
+                }
+        }
+    }
 
     fun createHotWallet() {
         viewModelScope.launch {
@@ -52,4 +66,5 @@ class UnAssistedIntroViewModel @Inject constructor(
 data class UnAssistedIntroState(
     val isLoading: Boolean = false,
     val openMainScreen: Boolean = false,
+    val plan: MembershipPlan = MembershipPlan.NONE,
 )
