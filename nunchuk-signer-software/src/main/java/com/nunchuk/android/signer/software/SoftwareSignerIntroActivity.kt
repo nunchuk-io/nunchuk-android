1ac5c9d0712ca13a02c4a4e752c53e1bf160fde6
Jayce Luong <jayce@nunchuk.io>
2024-03-25 21:24:33 +0700

    NUN-5187: Update create new software key content
    (cherry picked from commit caee2b07608e1874b34ce5ac28a6c27c3bc04afa)

--- nunchuk-signer-software/src/main/java/com/nunchuk/android/signer/software/SoftwareSignerIntroActivity.kt
+++ nunchuk-signer-software/src/main/java/com/nunchuk/android/signer/software/SoftwareSignerIntroActivity.kt
@@ -28,12 +28,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -129,9 +130,7 @@ fun SoftwareSignerIntroScreen(
                 title = stringResource(R.string.nc_before_you_start),
                 textStyle = NunchukTheme.typography.titleLarge,
                 actions = {
-                    IconButton(onClick = { }) {
-
-                    }
+                    Spacer(modifier = Modifier.size(40.dp))
                 }
             )
         },
