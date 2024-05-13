aedcea5da93834e57f66fc1bf2986c50c91e2f99
Jayce <jayce@nunchuk.io>
2024-05-13 15:57:20 +0700

    NUN-5397: Handle delete selected server

--- nunchuk-domain/src/main/java/com/nunchuk/android/model/StateEvent.kt
+++ nunchuk-domain/src/main/java/com/nunchuk/android/model/StateEvent.kt
@@ -2,5 +2,6 @@ package com.nunchuk.android.model
 
 sealed interface StateEvent {
     data class String(val data: kotlin.String) : StateEvent
+    data object Unit : StateEvent
     data object None : StateEvent
 }
