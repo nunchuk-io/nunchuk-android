02b8a14a2c95553e48735269322a204848bf3d0c
Jayce Luong <jayce@nunchuk.io>
2024-05-25 17:10:15 +0700

    NUN-5455: Handle reuse key
    (cherry picked from commit c5267fed53bcba1df65008ee809543ed5784fbfa)

--- nunchuk-main/src/main/java/com/nunchuk/android/main/membership/replacekey/ReplaceKeysViewModel.kt
+++ nunchuk-main/src/main/java/com/nunchuk/android/main/membership/replacekey/ReplaceKeysViewModel.kt
@@ -5,19 +5,29 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.nunchuk.android.core.account.AccountManager
 import com.nunchuk.android.core.domain.key.SetReplacingKeyXfpUseCase
+import com.nunchuk.android.core.mapper.MasterSignerMapper
 import com.nunchuk.android.core.signer.SignerModel
 import com.nunchuk.android.core.signer.toModel
+import com.nunchuk.android.core.util.isRecommendedPath
+import com.nunchuk.android.core.util.orUnknownError
+import com.nunchuk.android.main.membership.byzantine.addKey.AddKeyListEvent
 import com.nunchuk.android.model.ByzantineGroup
 import com.nunchuk.android.model.ByzantineMember
+import com.nunchuk.android.model.Result
+import com.nunchuk.android.model.SingleSigner
 import com.nunchuk.android.model.StateEvent
 import com.nunchuk.android.model.VerifyType
 import com.nunchuk.android.model.byzantine.AssistedWalletRole
 import com.nunchuk.android.model.byzantine.toRole
+import com.nunchuk.android.type.SignerTag
 import com.nunchuk.android.type.SignerType
+import com.nunchuk.android.usecase.UpdateRemoteSignerUseCase
 import com.nunchuk.android.usecase.byzantine.GetGroupUseCase
 import com.nunchuk.android.usecase.replace.FinalizeReplaceKeyUseCase
 import com.nunchuk.android.usecase.replace.GetReplaceWalletStatusUseCase
 import com.nunchuk.android.usecase.replace.InitReplaceKeyUseCase
+import com.nunchuk.android.usecase.replace.ReplaceKeyUseCase
+import com.nunchuk.android.usecase.signer.GetAllSignersUseCase
 import com.nunchuk.android.usecase.wallet.GetWalletDetail2UseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -37,17 +47,22 @@ class ReplaceKeysViewModel @Inject constructor(
     private val getReplaceWalletStatusUseCase: GetReplaceWalletStatusUseCase,
     private val finalizeReplaceKeyUseCase: FinalizeReplaceKeyUseCase,
     private val initReplaceKeyUseCase: InitReplaceKeyUseCase,
+    private val getAllSignersUseCase: GetAllSignersUseCase,
+    private val masterSignerMapper: MasterSignerMapper,
+    private val replaceKeyUseCase: ReplaceKeyUseCase,
+    private val updateRemoteSignerUseCase: UpdateRemoteSignerUseCase,
     private val savedStateHandle: SavedStateHandle
 ) : ViewModel() {
     private val args = ReplaceKeysFragmentArgs.fromSavedStateHandle(savedStateHandle)
     private val _uiState = MutableStateFlow(ReplaceKeysUiState())
     val uiState = _uiState.asStateFlow()
+    private val singleSigners = mutableListOf<SingleSigner>()
 
     init {
         viewModelScope.launch {
             getWalletDetail2UseCase(args.walletId).onSuccess { wallet ->
                 _uiState.update {
-                    it.copy(signers = wallet.signers.filter { signer -> signer.type != SignerType.SERVER }
+                    it.copy(walletSigners = wallet.signers.filter { signer -> signer.type != SignerType.SERVER }
                         .map { signer -> signer.toModel() })
                 }
             }
@@ -66,6 +81,23 @@ class ReplaceKeysViewModel @Inject constructor(
             }
         }
         getReplaceWalletStatus()
+        loadSigners()
+    }
+
+    private fun loadSigners() {
+        viewModelScope.launch {
+            getAllSignersUseCase(false).onSuccess { pair ->
+                val singleSigner = pair.second.distinctBy { it.masterFingerprint }
+                singleSigners.apply {
+                    clear()
+                    addAll(singleSigner)
+                }
+                val signers = pair.first.map { signer ->
+                    masterSignerMapper(signer)
+                } + singleSigner.map { signer -> signer.toModel() }
+                _uiState.update { it.copy(signers = signers) }
+            }
+        }
     }
 
     fun getReplaceWalletStatus() {
@@ -85,6 +117,8 @@ class ReplaceKeysViewModel @Inject constructor(
                         verifiedSigners = verifiedSigners
                     )
                 }
+            }.onFailure {
+                _uiState.update { it.copy(message = it.message) }
             }
         }
     }
@@ -124,8 +158,92 @@ class ReplaceKeysViewModel @Inject constructor(
         }
     }
 
+
+    fun getTapSigners() =
+        _uiState.value.signers.filter { it.type == SignerType.NFC && isSignerExist(it.fingerPrint).not() }
+
+    fun getColdcard() = _uiState.value.signers.filter {
+        (it.type == SignerType.COLDCARD_NFC
+                && it.derivationPath.isRecommendedPath
+                && isSignerExist(it.fingerPrint).not()) || (it.type == SignerType.AIRGAP && it.tags.isEmpty())
+    }
+
+    fun getAirgap(tag: SignerTag?): List<SignerModel> {
+        return if (tag == null) {
+            _uiState.value.signers.filter {
+                it.type == SignerType.AIRGAP
+                        && isSignerExist(it.fingerPrint).not()
+            }
+        } else {
+            _uiState.value.signers.filter {
+                it.type == SignerType.AIRGAP
+                        && isSignerExist(it.fingerPrint).not()
+                        && (it.tags.contains(tag) || it.tags.isEmpty())
+            }
+        }
+    }
+
+    fun getHardwareSigners(tag: SignerTag) =
+        _uiState.value.signers.filter { it.type == SignerType.HARDWARE && it.tags.contains(tag) }
+
+    fun getSoftwareSigners() =
+        _uiState.value.signers.filter {
+            (it.type == SignerType.SOFTWARE || it.type == SignerType.FOREIGN_SOFTWARE) && isSignerExist(
+                it.fingerPrint
+            ).not()
+        }
+
     fun markOnCreateWalletSuccess() {
         _uiState.update { it.copy(createWalletSuccess = StateEvent.None) }
+    }
+
+    private fun isSignerExist(masterSignerId: String) =
+        _uiState.value.replaceSigners.containsKey(masterSignerId)
+                && _uiState.value.walletSigners.any { it.fingerPrint == masterSignerId }
+
+    fun getCountWalletSoftwareSignersInDevice(): Int {
+        val replaceSigners = _uiState.value.replaceSigners
+        return _uiState.value.walletSigners.count {
+            val signer = replaceSigners[it.fingerPrint] ?: it
+            (signer.type == SignerType.SOFTWARE && signer.isVisible)
+        }
+    }
+
+    fun onUpdateSignerTag(signer: SignerModel, tag: SignerTag) {
+        viewModelScope.launch {
+            singleSigners.find { it.masterFingerprint == signer.fingerPrint && it.derivationPath == signer.derivationPath }
+                ?.let { singleSigner ->
+                    val result =
+                        updateRemoteSignerUseCase.execute(singleSigner.copy(tags = listOf(tag)))
+                    if (result is Result.Success) {
+                        loadSigners()
+                        onReplaceKey(singleSigner.copy(tags = listOf(tag)))
+                    } else {
+                        _uiState.update { it.copy(message = (result as Result.Error).exception.message.orUnknownError()) }
+                    }
+                }
+        }
+    }
+
+    fun onReplaceKey(signer: SingleSigner) {
+        viewModelScope.launch {
+            replaceKeyUseCase(
+                ReplaceKeyUseCase.Param(
+                    groupId = args.groupId,
+                    walletId = args.walletId,
+                    xfp = savedStateHandle.get<String>(REPLACE_XFP).orEmpty(),
+                    signer = signer
+                )
+            ).onSuccess {
+                getReplaceWalletStatus()
+            }.onFailure {
+                _uiState.update { it.copy(message = it.message) }
+            }
+        }
+    }
+
+    fun onHandledMessage() {
+        _uiState.update { it.copy(message = "") }
     }
 
     val replacedXfp: String
@@ -137,10 +255,12 @@ class ReplaceKeysViewModel @Inject constructor(
 }
 
 data class ReplaceKeysUiState(
-    val signers: List<SignerModel> = emptyList(),
+    val walletSigners: List<SignerModel> = emptyList(),
     val replaceSigners: Map<String, SignerModel> = emptyMap(),
     val verifiedSigners: Set<String> = emptySet(),
     val group: ByzantineGroup? = null,
     val myRole: AssistedWalletRole = AssistedWalletRole.NONE,
-    val createWalletSuccess: StateEvent = StateEvent.None
+    val createWalletSuccess: StateEvent = StateEvent.None,
+    val signers: List<SignerModel> = emptyList(),
+    val message: String = ""
 )
