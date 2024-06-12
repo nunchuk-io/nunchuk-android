f78991a9cc248f63c0ca0dc5f2a97515dbd5659f
Jayce Luong <jayce@nunchuk.io>
2024-06-12 08:20:26 +0700

    NUN-5507: Fix UI issue
    (cherry picked from commit 1a2629096df808ed4eb782f20317b82db4f85ec8)

--- nunchuk-core/src/main/java/com/nunchuk/android/compose/NcScaffold.kt
+++ nunchuk-core/src/main/java/com/nunchuk/android/compose/NcScaffold.kt
@@ -6,7 +6,6 @@ import androidx.compose.material3.FabPosition
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.ScaffoldDefaults
-import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
--- nunchuk-main/src/main/java/com/nunchuk/android/main/membership/wallet/TransferFundFragment.kt
+++ nunchuk-main/src/main/java/com/nunchuk/android/main/membership/wallet/TransferFundFragment.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Checkbox
@@ -121,6 +122,7 @@ private fun TransferFundContent(
     var isRemoveUnusedKeys by rememberSaveable { mutableStateOf(false) }
     NunchukTheme {
         NcScaffold(
+            modifier = Modifier.systemBarsPadding(),
             topBar = {
                 NcTopAppBar(title = "")
             },
