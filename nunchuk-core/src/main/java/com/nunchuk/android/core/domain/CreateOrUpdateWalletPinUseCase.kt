dde2e3f0484bf6138a0ad59374bad8cd3b83d8fa
Jayce Luong <jayce@nunchuk.io>
2024-09-30 15:02:06 +0700

    NUN-6221: Display wrong setting ON/OFF PIN and Password or passphrase after creating decoy wallet successfully

--- nunchuk-core/src/main/java/com/nunchuk/android/core/domain/CreateOrUpdateWalletPinUseCase.kt
+++ nunchuk-core/src/main/java/com/nunchuk/android/core/domain/CreateOrUpdateWalletPinUseCase.kt
@@ -22,19 +22,32 @@ package com.nunchuk.android.core.domain
 import com.nunchuk.android.domain.di.IoDispatcher
 import com.nunchuk.android.nativelib.NunchukNativeSdk
 import com.nunchuk.android.repository.SettingRepository
+import com.nunchuk.android.usecase.GetWalletSecuritySettingUseCase
+import com.nunchuk.android.usecase.UpdateWalletSecuritySettingUseCase
 import com.nunchuk.android.usecase.UseCase
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class CreateOrUpdateWalletPinUseCase @Inject constructor(
     @IoDispatcher ioDispatcher: CoroutineDispatcher,
     private val settingRepository: SettingRepository,
     private val androidNativeSdk: NunchukNativeSdk,
+    private val updateWalletSecuritySettingUseCase: UpdateWalletSecuritySettingUseCase,
+    private val getWalletSecuritySettingUseCase: GetWalletSecuritySettingUseCase,
 ) : UseCase<String, Unit>(ioDispatcher) {
 
     override suspend fun execute(parameters: String) {
         if (parameters.isNotBlank()) {
             settingRepository.setWalletPin(androidNativeSdk.hashSHA256(parameters))
+            val walletSecuritySetting = getWalletSecuritySettingUseCase(Unit).map { it.getOrThrow() }.first()
+            updateWalletSecuritySettingUseCase(
+                walletSecuritySetting.copy(
+                    protectWalletPassphrase = false,
+                    protectWalletPassword = false
+                )
+            )
         } else {
             settingRepository.setWalletPin("")
         }
