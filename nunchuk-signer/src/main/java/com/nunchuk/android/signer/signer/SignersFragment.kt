2a3be34728623baf38e0f1e3dad5fc36865c2f4b
Jayce Luong <jayce@nunchuk.io>
2024-10-05 16:14:30 +0700

    NUN-6241: Update "Add key" shortcut

--- nunchuk-signer/src/main/java/com/nunchuk/android/signer/signer/SignersFragment.kt
+++ nunchuk-signer/src/main/java/com/nunchuk/android/signer/signer/SignersFragment.kt
@@ -4,21 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,10 +29,8 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.compose.content
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.nunchuk.android.compose.NcCircleImage
 import com.nunchuk.android.compose.NcScaffold
 import com.nunchuk.android.compose.NunchukTheme
-import com.nunchuk.android.compose.greyLight
 import com.nunchuk.android.compose.provider.SignersModelProvider
 import com.nunchuk.android.compose.signer.SignerCard
 import com.nunchuk.android.core.signer.SignerModel
@@ -99,12 +98,21 @@ fun SignersContent(
                     title = {
                         Text(
                             text = stringResource(R.string.nc_title_signers),
-                            style = NunchukTheme.typography.title,
+                            style = NunchukTheme.typography.titleLarge,
                             color = MaterialTheme.colorScheme.onPrimary,
                             textAlign = TextAlign.Center,
                             modifier = Modifier.fillMaxWidth(),
                         )
                     },
+                    actions = {
+                        IconButton(onClick = onAddSignerClick) {
+                            Icon(
+                                painterResource(id = R.drawable.ic_add_dark),
+                                contentDescription = "Add Signer",
+                                tint = Color.White
+                            )
+                        }
+                    }
                 )
             }
         ) { innerPadding ->
@@ -113,26 +121,6 @@ fun SignersContent(
                     .padding(innerPadding)
                     .padding(vertical = 4.dp),
             ) {
-                item("add_signer") {
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable(onClick = onAddSignerClick)
-                            .padding(12.dp),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        NcCircleImage(
-                            resId = R.drawable.ic_add_dark,
-                            color = MaterialTheme.colorScheme.greyLight
-                        )
-                        Text(
-                            text = stringResource(R.string.nc_add_key),
-                            style = NunchukTheme.typography.title,
-                            modifier = Modifier.padding(start = 12.dp),
-                        )
-                    }
-
-                }
                 items(uiState.signers.orEmpty()) { signer ->
                     SignerCard(item = signer, onSignerSelected = onSignerClick)
                 }
