7f88890e85daff94e5cf2a96cf2ff479ade5834d
Thong Le <thongle@nunchuk.io>
2024-12-18 15:07:10 +0700

    NUN-6364: Total balance view

--- nunchuk-main/src/main/java/com/nunchuk/android/main/components/tabs/wallet/totalbalance/TotalBalanceView.kt
+++ nunchuk-main/src/main/java/com/nunchuk/android/main/components/tabs/wallet/totalbalance/TotalBalanceView.kt
@@ -1,15 +1,16 @@
 package com.nunchuk.android.main.components.tabs.wallet.totalbalance
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -25,13 +26,14 @@ fun TotalBalanceView(
     Row(
         modifier = Modifier
             .fillMaxWidth()
+            .background(colorResource(id = R.color.nc_grey_light))
             .padding(16.dp),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
             text = stringResource(R.string.nc_total_balance),
-            style = if (isLargeFont) NunchukTheme.typography.title else MaterialTheme.typography.titleSmall,
+            style = if (isLargeFont) NunchukTheme.typography.title else NunchukTheme.typography.titleSmall,
             modifier = Modifier.align(Alignment.CenterVertically)
         )
 
@@ -40,13 +42,13 @@ fun TotalBalanceView(
         ) {
             Text(
                 text = balanceSatoshis,
-                style = if (isLargeFont) NunchukTheme.typography.title else MaterialTheme.typography.titleSmall,
+                style = if (isLargeFont) NunchukTheme.typography.title else NunchukTheme.typography.titleSmall,
                 textAlign = TextAlign.End
             )
 
             Text(
                 text = balanceDollars,
-                style = if (isLargeFont) NunchukTheme.typography.body else MaterialTheme.typography.bodySmall,
+                style = if (isLargeFont) NunchukTheme.typography.body else NunchukTheme.typography.bodySmall,
                 textAlign = TextAlign.End
             )
         }
