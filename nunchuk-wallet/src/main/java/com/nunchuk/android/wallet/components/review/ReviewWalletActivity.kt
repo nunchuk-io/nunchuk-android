776bb63e46c276045aa074da706098e2d5a76f3b
Jayce Luong <jayce@nunchuk.io>
2025-01-01 18:15:19 +0700

    NUN-6716: Create wallet new UI

--- nunchuk-wallet/src/main/java/com/nunchuk/android/wallet/components/review/ReviewWalletActivity.kt
+++ nunchuk-wallet/src/main/java/com/nunchuk/android/wallet/components/review/ReviewWalletActivity.kt
@@ -22,18 +22,53 @@ package com.nunchuk.android.wallet.components.review
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import com.nunchuk.android.core.base.BaseActivity
-import com.nunchuk.android.core.util.isTaproot
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
+import com.nunchuk.android.compose.NcIcon
+import com.nunchuk.android.compose.NcPrimaryDarkButton
+import com.nunchuk.android.compose.NcScaffold
+import com.nunchuk.android.compose.NcTopAppBar
+import com.nunchuk.android.compose.NunchukTheme
+import com.nunchuk.android.compose.backgroundMidGray
+import com.nunchuk.android.compose.provider.SignersModelProvider
+import com.nunchuk.android.compose.signer.SignerCard
+import com.nunchuk.android.compose.textPrimary
+import com.nunchuk.android.core.base.BaseComposeActivity
+import com.nunchuk.android.core.signer.SignerModel
 import com.nunchuk.android.nav.args.ReviewWalletArgs
-import com.nunchuk.android.share.wallet.bindWalletConfiguration
+import com.nunchuk.android.type.AddressType
+import com.nunchuk.android.type.WalletType
 import com.nunchuk.android.utils.viewModelProviderFactoryOf
-import com.nunchuk.android.wallet.components.config.SignersViewBinder
+import com.nunchuk.android.wallet.R
 import com.nunchuk.android.wallet.components.review.ReviewWalletEvent.CreateWalletErrorEvent
 import com.nunchuk.android.wallet.components.review.ReviewWalletEvent.CreateWalletSuccessEvent
 import com.nunchuk.android.wallet.components.review.ReviewWalletEvent.SetLoadingEvent
-import com.nunchuk.android.wallet.databinding.ActivityReviewWalletBinding
 import com.nunchuk.android.wallet.util.isWalletExisted
 import com.nunchuk.android.wallet.util.toReadableString
 import com.nunchuk.android.widget.NCToastMessage
@@ -41,7 +76,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class ReviewWalletActivity : BaseActivity<ActivityReviewWalletBinding>() {
+class ReviewWalletActivity : BaseComposeActivity() {
 
     @Inject
     internal lateinit var vmFactory: ReviewWalletViewModel.Factory
@@ -54,14 +89,27 @@ class ReviewWalletActivity : BaseActivity<ActivityReviewWalletBinding>() {
 
     private val args: ReviewWalletArgs by lazy { ReviewWalletArgs.deserializeFrom(intent) }
 
-    override fun initializeBinding() = ActivityReviewWalletBinding.inflate(layoutInflater).also {
-        enableEdgeToEdge()
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setupViews()
+        enableEdgeToEdge()
+
+        setContent {
+            ReviewWalletContent(
+                args = args,
+                signers = viewModel.mapSigners()
+            ) {
+                viewModel.handleContinueEvent(
+                    walletName = args.walletName,
+                    walletType = args.walletType,
+                    addressType = args.addressType,
+                    totalRequireSigns = args.totalRequireSigns,
+                    signers = args.signers,
+                    decoyPin = args.decoyPin
+                )
+            }
+        }
+
         observeEvent()
     }
 
@@ -95,38 +143,6 @@ class ReviewWalletActivity : BaseActivity<ActivityReviewWalletBinding>() {
         )
     }
 
-    private fun setupViews() {
-        binding.walletName.text = args.walletName
-        val signers = viewModel.mapSigners()
-        binding.configuration.bindWalletConfiguration(
-            requireSigns = args.totalRequireSigns,
-            totalSigns = signers.size
-        )
-
-        binding.walletType.text = args.walletType.toReadableString(this)
-        binding.addressType.text = args.addressType.toReadableString(this)
-        SignersViewBinder(
-            container = binding.signersContainer,
-            signers = signers,
-            isTaproot = args.addressType.isTaproot(),
-            totalRequiredSign = args.totalRequireSigns
-        ).bindItems()
-
-        binding.btnContinue.setOnClickListener {
-            viewModel.handleContinueEvent(
-                walletName = args.walletName,
-                walletType = args.walletType,
-                addressType = args.addressType,
-                totalRequireSigns = args.totalRequireSigns,
-                signers = args.signers,
-                decoyPin = args.decoyPin
-            )
-        }
-        binding.toolbar.setNavigationOnClickListener {
-            finish()
-        }
-    }
-
     companion object {
 
         fun start(
@@ -140,5 +156,130 @@ class ReviewWalletActivity : BaseActivity<ActivityReviewWalletBinding>() {
             )
         }
     }
+}
 
+@Composable
+fun ReviewWalletContent(
+    args: ReviewWalletArgs,
+    signers: List<SignerModel>,
+    onContinue: () -> Unit = {}
+) {
+    NunchukTheme {
+        NcScaffold(
+            modifier = Modifier.navigationBarsPadding(),
+            bottomBar = {
+                NcPrimaryDarkButton(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth(),
+                    onClick = onContinue
+                ) {
+                    Text(text = stringResource(id = R.string.nc_wallet_create_wallet))
+                }
+            },
+            topBar = {
+                Column(
+                    modifier = Modifier
+                        .background(MaterialTheme.colorScheme.backgroundMidGray)
+                        .systemBarsPadding()
+                        .padding(bottom = 24.dp)
+                ) {
+                    NcTopAppBar(
+                        title = stringResource(id = R.string.nc_wallet_review_wallet_title),
+                        textStyle = NunchukTheme.typography.titleLarge,
+                        backgroundColor = MaterialTheme.colorScheme.backgroundMidGray,
+                    )
+
+                    Text(
+                        text = args.walletName,
+                        modifier = Modifier
+                            .padding(horizontal = 16.dp)
+                            .padding(top = 16.dp),
+                        style = NunchukTheme.typography.heading
+                    )
+
+                    Row(
+                        modifier = Modifier
+                            .padding(horizontal = 16.dp)
+                            .padding(top = 12.dp),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        Text(
+                            modifier = Modifier
+                                .background(
+                                    MaterialTheme.colorScheme.background,
+                                    shape = RoundedCornerShape(20.dp)
+                                )
+                                .padding(horizontal = 10.dp, vertical = 4.dp),
+                            text = if (args.walletType == WalletType.SINGLE_SIG) {
+                                stringResource(id = R.string.nc_wallet_single_sig)
+                            } else {
+                                "${args.totalRequireSigns}/${args.signers.size} ${stringResource(id = R.string.nc_wallet_multisig)}"
+                            },
+                            style = NunchukTheme.typography.caption
+                        )
+
+                        Text(
+                            modifier = Modifier
+                                .background(
+                                    MaterialTheme.colorScheme.background,
+                                    shape = RoundedCornerShape(20.dp)
+                                )
+                                .padding(horizontal = 10.dp, vertical = 4.dp),
+                            text = args.addressType.toReadableString(LocalContext.current),
+                            style = NunchukTheme.typography.caption
+                        )
+                    }
+                }
+            }
+        ) { innerPadding ->
+            LazyColumn(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize(),
+                contentPadding = PaddingValues(16.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                item("key") {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        NcIcon(
+                            modifier = Modifier.size(24.dp),
+                            painter = painterResource(id = R.drawable.ic_mulitsig_dark),
+                            contentDescription = "Key",
+                            tint = MaterialTheme.colorScheme.textPrimary
+                        )
+
+                        Text(
+                            text = stringResource(id = R.string.nc_title_signers),
+                            style = NunchukTheme.typography.body
+                        )
+                    }
+                }
+
+                items(signers) {
+                    SignerCard(item = it)
+                }
+            }
+        }
+    }
+}
+
+@PreviewLightDark
+@Composable
+private fun PreviewReviewWalletContent(
+    @PreviewParameter(SignersModelProvider::class) signers: List<SignerModel>,
+) {
+    ReviewWalletContent(
+        args = ReviewWalletArgs(
+            walletName = "Wallet Name",
+            walletType = WalletType.SINGLE_SIG,
+            addressType = AddressType.LEGACY,
+            totalRequireSigns = 1,
+            signers = emptyList()
+        ),
+        signers = signers
+    )
 }
