cb26b55c577a8bb20efc6cb4078f8e498632f598
Thong Le <thongle@nunchuk.io>
2025-02-12 21:27:37 +0700

    [NUN-7092] Update Cancel recovery group wallet

--- nunchuk-main/src/main/java/com/nunchuk/android/main/groupwallet/recover/FreeGroupWalletRecoverScreen.kt
+++ nunchuk-main/src/main/java/com/nunchuk/android/main/groupwallet/recover/FreeGroupWalletRecoverScreen.kt
@@ -5,11 +5,13 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -17,13 +19,11 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -34,6 +34,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalViewConfiguration
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -174,7 +175,7 @@ fun FreeGroupWalletRecoverScreen(
                 modifier = Modifier,
                 colors = TopAppBarDefaults.centerAlignedTopAppBarColors(containerColor = MaterialTheme.colorScheme.background),
                 navigationIcon = {
-                    IconButton(onClick = { onBackPressOwner?.onBackPressedDispatcher?.onBackPressed() }) {
+                    IconButton(onClick = { showAskForDeleteDialog = true }) {
                         Icon(
                             painter = painterResource(id = R.drawable.ic_close),
                             contentDescription = "Back",
@@ -214,16 +215,7 @@ fun FreeGroupWalletRecoverScreen(
 
                 },
                 actions = {
-                    CompositionLocalProvider(LocalContentColor provides MaterialTheme.colorScheme.textPrimary) {
-                        IconButton(onClick = {
-                            showMoreOption = true
-                        }) {
-                            Icon(
-                                painter = painterResource(id = R.drawable.ic_more),
-                                contentDescription = "More icon"
-                            )
-                        }
-                    }
+                    Spacer(modifier = Modifier.size(LocalViewConfiguration.current.minimumTouchTargetSize))
                 }
             )
         },
