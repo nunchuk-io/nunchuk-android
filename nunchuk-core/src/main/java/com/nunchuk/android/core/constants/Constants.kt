7c723a5108e7a606a61a906fe47d65a2c6d544e2
Jayce Luong <jayce@nunchuk.io>
2025-05-19 20:50:46 +0700

    NUN-7766: Missing /tx on Custom blockchain explorer screen for Signer server on

--- nunchuk-core/src/main/java/com/nunchuk/android/core/constants/Constants.kt
+++ nunchuk-core/src/main/java/com/nunchuk/android/core/constants/Constants.kt
@@ -28,7 +28,7 @@ object Constants {
     const val TESTNET_URL_TEMPLATE = "https://mempool.space/testnet4/tx/"
     const val MAINNET_URL_TEMPLATE = "https://mempool.space/tx/"
     const val BLOCKSTREAM_MAINNET_ADDRESS_TEMPLATE = "https://mempool.space/address/"
-    const val GLOBAL_SIGNET_EXPLORER = "https://mempool.space/signet"
+    const val GLOBAL_SIGNET_EXPLORER = "https://mempool.space/signet/tx/"
 
 }
 
