6473759239abc3ed372f42a53bab5fb06894209c
Thong Le <thongle@nunchuk.io>
2025-07-24 23:27:24 +0700

    [NUN-8136] Can't create a support chat after changing the email address

--- nunchuk-core/src/main/java/com/nunchuk/android/core/repository/SettingRepositoryImpl.kt
+++ nunchuk-core/src/main/java/com/nunchuk/android/core/repository/SettingRepositoryImpl.kt
@@ -23,7 +23,9 @@ import com.google.gson.Gson
 import com.nunchuk.android.core.account.AccountManager
 import com.nunchuk.android.core.persistence.NcDataStore
 import com.nunchuk.android.core.persistence.NcEncryptedPreferences
+import com.nunchuk.android.model.BannerState
 import com.nunchuk.android.model.MembershipPlan
+import com.nunchuk.android.model.WalletBannerState
 import com.nunchuk.android.model.campaigns.Campaign
 import com.nunchuk.android.model.campaigns.ReferrerCode
 import com.nunchuk.android.model.setting.BiometricConfig
@@ -31,8 +33,6 @@ import com.nunchuk.android.model.setting.HomeDisplaySetting
 import com.nunchuk.android.model.setting.TaprootFeeSelectionSetting
 import com.nunchuk.android.model.setting.WalletSecuritySetting
 import com.nunchuk.android.model.wallet.WalletOrder
-import com.nunchuk.android.model.BannerState
-import com.nunchuk.android.model.WalletBannerState
 import com.nunchuk.android.persistence.dao.WalletOrderDao
 import com.nunchuk.android.persistence.entity.toDomain
 import com.nunchuk.android.persistence.entity.toEntity
