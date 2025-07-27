ad04db89c89d421f12e8ca9ec5583400ac6f50cd
Thong Le <thongle@nunchuk.io>
2025-07-27 22:32:51 +0700

    Update logic filter signer type

--- nunchuk-app/src/main/java/com/nunchuk/android/app/miniscript/MiniscriptSharedWalletViewModel.kt
+++ nunchuk-app/src/main/java/com/nunchuk/android/app/miniscript/MiniscriptSharedWalletViewModel.kt
@@ -581,7 +581,8 @@ class MiniscriptSharedWalletViewModel @Inject constructor(
 
     fun getSuggestedSigners(): List<SupportedSigner> {
         return _uiState.value.let { state ->
-            state.supportedTypes.takeIf { state.addressType.isTaproot() == true }.orEmpty()
+            state.supportedTypes.takeIf { state.addressType.isTaproot() == true }
+                ?.filter { it.walletType == WalletType.MULTI_SIG || it.walletType == null }.orEmpty()
         }
     }
 
