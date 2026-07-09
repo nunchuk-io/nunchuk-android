aec0bdb1045a2a574e8c2e698b88909999f26adf
Jayce Luong <jayce@nunchuk.io>
2026-07-09 10:09:39 +0700

    Revert "[NUN-10043] [USDT] Missing “You need more LBTC to pay this transaction” banner and red highlight on fee field when setting fee on the Confirm transaction screen"
    This reverts commit c907f643492b39058374a1253670a63057a5c06b.

--- nunchuk-transaction/src/main/java/com/nunchuk/android/transaction/components/send/confirmation/TransactionConfirmViewModel.kt
+++ nunchuk-transaction/src/main/java/com/nunchuk/android/transaction/components/send/confirmation/TransactionConfirmViewModel.kt
@@ -302,18 +302,7 @@ class TransactionConfirmViewModel @Inject constructor(
                 _event.emit(CreateTxErrorEvent(it.message.orUnknownError(), it.nativeErrorCode()))
                 return@launch
             }
-            // The LBTC balance must cover any LBTC being sent plus the fee (also paid in LBTC).
-            // USDT sends have no LBTC outflow, so this reduces to the fee-only check.
-            val usdtAssetId = _state.value.usdtAssetId
-            val lbtcOutflow = txReceipts
-                .filter { it.tokenAssetId != usdtAssetId }
-                .sumOf { it.amount.toAmount().value }
-            val requiredLbtc = if (subtractFeeFromAmount) {
-                lbtcOutflow
-            } else {
-                lbtcOutflow + estimatedFee.value
-            }
-            if (lbtcBalance.value < requiredLbtc) {
+            if (lbtcBalance.value < estimatedFee.value) {
                 showNotEnoughLbtcForFee(estimatedFee, feeRate)
                 return@launch
             }
